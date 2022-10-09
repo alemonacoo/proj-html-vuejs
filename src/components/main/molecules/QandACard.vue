eb21f6700ab71313f06fadd6fd812fe31a1c21f5
alemonacoo <ale.monaco@outlook.com>
2022-10-09 18:14:11 +0200

    adding fifth section: Q and A's

---
+++ src/components/main/molecules/QandACard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="QandA">
+    <div class="icon-container" :class="status ? 'active' : ''">
+      <p v-if="status">A</p>
+      <p v-else>C</p>
+    </div>
+    <div class="text-container" :class="status ? 'active' : ''">
+      <h4>
+        {{ question }}
+      </h4>
+      <div class="answer-section" v-if="status">
+        <p>{{ answer }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QandACard",
+  props: {
+    question: String,
+    answer: String,
+    status: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/main.scss";
+.QandA {
+  @include flex-row;
+  gap: 10px;
+  padding: 20px 0px;
+  .text-container {
+    @include flex-col;
+    gap: 8px;
+    p {
+      font-size: 0.8rem;
+    }
+  }
+}
+.active {
+  color: $jungle_green;
+}
+.answer-section {
+  color: white;
+}
+.QandA:hover {
+  cursor: pointer;
+  .text-container {
+    h4 {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- src/components/main/organisms/QandASection.vue
+++ src/components/main/organisms/QandASection.vue
@@ -1,13 +1,112 @@
 <template>
-  <div class="QandA-section"></div>
+  <div class="QandA-section">
+    <img
+      id="wallpaper-world"
+      src="@/assets/img/background-map-dots-top-right-transparent.png"
+      alt="wallpaperNotFound"
+    />
+    <img
+      id="wallpaper-square"
+      src="@/assets/img/maxcoach-shape-07.png"
+      alt="wallpaperNotFound"
+    />
+    <div class="left-side">
+      <img src="@/assets/img/home-business-video-poster-670x450.jpg" alt="" />
+      <img id="youtube" src="@/assets/img/icon-youtube-play.png" alt="" />
+    </div>
+    <div class="right-side">
+      <h2>To Thrive In Business Today, You'll Need A Good Plan</h2>
+      <div>
+        <div
+          v-for="(QandA, index) in QandAs"
+          :key="index"
+          @click="ActivateQandA(index)"
+        >
+          <QandACard
+            :question="QandA.question"
+            :answer="QandA.answer"
+            :status="ActiveIndex == index ? true : false"
+          />
+          <hr />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import QandAs from "@/data/QandA.json";
+import QandACard from "@/components/main/molecules/QandACard.vue";
+
 export default {
   name: "QandASection",
+  components: {
+    QandACard,
+  },
+  data() {
+    return { QandAs, ActiveIndex: -1 };
+  },
+  methods: {
+    ActivateQandA(i) {
+      if (i === this.ActiveIndex) {
+        return (this.ActiveIndex = -1);
+      }
+      console.log(i);
+      return (this.ActiveIndex = i);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/main.scss";
+.QandA-section {
+  @include flex-row;
+  position: relative;
+  justify-content: center;
+  padding: 5% 10%;
+  height: 80vh;
+  background-color: $tuna;
+  #wallpaper-world {
+    position: absolute;
+    width: 56%;
+    top: 0;
+    right: 0;
+  }
+  #wallpaper-square {
+    position: absolute;
+    width: 113px;
+    bottom: 10px;
+    left: 30px;
+  }
+  .left-side {
+    @include flex-row;
+    height: 100%;
+    width: 45%;
+    position: relative;
+    img {
+      width: 100%;
+      object-fit: contain;
+      border-radius: 5px;
+    }
+    #youtube {
+      position: absolute;
+      height: 50px;
+      top: calc(50% - 25px);
+    }
+  }
+  .right-side {
+    position: relative;
+    z-index: 1;
+    padding: 30px;
+    width: 50%;
+    @include flex-col;
+    justify-content: space-evenly;
+    color: white;
+    hr {
+      border-top: none;
+      border: solid 1px rgba(255, 255, 255, 0.5);
+    }
+  }
+}
 </style>
